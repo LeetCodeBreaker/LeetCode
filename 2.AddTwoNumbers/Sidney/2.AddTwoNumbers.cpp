3eb60b013d51325b26fa2dad699769a352a14d82
SidneyLin <sidney781030@gmail.com>
2015-06-19 00:31:02 +0800

    forget assaign prenode

--- 2.AddTwoNumbers/Sidney/2.AddTwoNumbers.cpp
+++ 2.AddTwoNumbers/Sidney/2.AddTwoNumbers.cpp
@@ -28,55 +28,62 @@ class Solution {
 
     public:
 
-        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+        int value1, value2;
+
+        struct ListNode *preNode = NULL;
+        struct ListNode *HeadNode = NULL;
+        struct ListNode *FirstList = l1;
+        struct ListNode *SecondList = l2;
+
+        while(!(FirstList == NULL && SecondList == NULL)) 
         {
-            int value1, value2;
-            struct ListNode *preNode = NULL;
-            struct ListNode *HeadNode = NULL;
-            while(!(l1->next == NULL && l2->next == NULL)) 
-            {
-                if(l1->next == NULL) {
-                    value1 = 0;
-                }
-                else {
-                    value1 = l1->val;
-                }
-                if(l2->next == NULL) {
-                    value2 = 0;
-                }
-                else {
-                    value2 = l2->val;
-                }
-                struct ListNode *Node =  new ListNode(value1 + value2);
-                if(preNode != NULL) {
-                    preNode->next = Node;
-                }
-                else {
-                    HeadNode = Node;
-                }
-                preNode = Node;
+            if(FirstList == NULL) {
+                value1 = 0;
+            }
+            else {
+                value1 = FirstList->val;
+            }
+            if(SecondList == NULL) {
+                value2 = 0;
+            }
+            else {
+                value2 = SecondList->val;
             }
 
-            ListNode *NodeNow = HeadNode; 
-            while(NodeNow != NULL) 
-            {
-                int ret = (NodeNow->val) / 10;
-                NodeNow->val = (NodeNow->val) % 10;
-                if(NodeNow->next != NULL) {
-                    (NodeNow->next)->val += ret;
-                }
-                else {
-                    if(ret > 0) {
-                        NodeNow->next = new ListNode(ret);
-                    }
-                }
-                NodeNow = NodeNow->next;
+            struct ListNode *Node =  new ListNode(value1 + value2);
+
+            if(preNode != NULL) {
+                preNode->next = Node;
+            }
+            else {
+                HeadNode = Node;
             }
             
-            return HeadNode;
+            preNode = Node;
+
+            FirstList = FirstList->next;
+            SecondList = SecondList->next;
+        }
+
+        ListNode *NodeNow = HeadNode; 
+        while(NodeNow != NULL) 
+        {
+            int ret = (NodeNow->val) / 10;
+            NodeNow->val = (NodeNow->val) % 10;
+            if(NodeNow->next != NULL) {
+                (NodeNow->next)->val += r/et;
+            }
+            else {
+                if(ret > 0) {
+                    NodeNow->next = new ListNode(ret);
+                }
+            }
+
+            NodeNow = NodeNow->next;
 
         }
 
+        return HeadNode;
 };
 int main() 
 {
