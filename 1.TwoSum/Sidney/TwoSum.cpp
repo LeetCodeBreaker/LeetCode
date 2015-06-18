d113fe57697a46b6fe54595c84ee9df7ea4cf79d
SidneyLin <sidney781030@gmail.com>
2015-06-18 23:12:33 +0800

    OK version

--- 1.TwoSum/Sidney/TwoSum.cpp
+++ 1.TwoSum/Sidney/TwoSum.cpp
@@ -6,28 +6,37 @@ using namespace std;
 class Solution {
 
     public:
+        int find(vector<int>& nums, int target)
+        {
+            for(int i = 0; i < nums.size(); i++) {
+                if(nums[i] == target) {
+                    return i+1;
+                }
+            }
+        }
+        int find_reverse(vector<int>& nums, int target)
+        {
+            for(int i = nums.size()-1 ; i>=0; i--) {
+                if(nums[i] == target) {
+                    return i+1;
+                }
+            }
+        }
+
         vector<int> twoSum(vector<int>& nums, int target) 
         {
             vector<int> ans;
-            vector<vector<int> > table;
-            for(int i = 0; i < target; i++) {
-                table.push_back(vector<int>());
-            }
-            for(int i = 0; i < nums.size(); i++) {
-                if(nums[i] < target) {
-                    table[nums[i] % target].push_back(i);
-                }
-            }
+            vector<int> nums_sorted = nums;
 
-            for(int i = 1; i < target; i++) {
-                for(int j = 0; j < table[i].size(); j++) {
-                    for(int k = 0; k < table[target-i].size(); k++) {
-                        if (nums[table[i][j]] + nums[table[target-1-i][k]] == target) {
-                            ans.push_back(table[i][j]);
-                            ans.push_back(table[target-i][k]);
-                            sort(ans.begin(), ans.end());
-                            return ans;
-                        }
+            sort(nums_sorted.begin(), nums_sorted.end());
+
+            for(int i = 0; i < nums_sorted.size(); i++) {
+                for(int j = i+1; j < nums_sorted.size(); j++) {
+                    if(nums_sorted[i] + nums_sorted[j] == target) {
+                        ans.push_back(find(nums, nums_sorted[i]));
+                        ans.push_back(find_reverse(nums, nums_sorted[j]));
+                        sort(ans.begin(), ans.end());
+                        return ans;
                     }
                 }
             }
@@ -41,11 +50,13 @@ int main()
     vector<int> test;
     vector<int> ans;
     Solution sol;
+    test.push_back(3);
     test.push_back(2);
     test.push_back(4);
-    test.push_back(3);
     ans = sol.twoSum(test, 6);
     printf("%d %d\n",ans[0], ans[1]);
 
     return 0;
 }
+
+
