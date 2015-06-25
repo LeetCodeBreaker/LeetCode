6e3f40d4519cd46dc18551688438756cba06210a
fishead1116 <ddfish1116@gmail.com>
2015-06-25 09:45:28 +0800

    Create Solution.java

---
+++ 70.ClimbingStairs/Ddfish/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int climbStairs(int n) {
+        
+        if(n <=2){
+            return n;
+        }
+        
+        int[] stairs = new int[n+1];
+        stairs[0] = 0;
+        stairs[1] = 1;
+        stairs[2] = 2;
+        for(int i = 3 ; i <= n ;i++){
+            stairs[i] = stairs[i-1]+stairs[i-2];
+        }
+        
+        return stairs[n];
+        
+    }
+}
