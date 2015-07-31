333373799746e9d720277af30e1712c28dc61e21
clywin123 <clywin123@gmail.com>
2015-07-31 17:19:29 +0800

    Create search.py

---
+++ 081.Search-in-Rotated-Sorted-Array-II/clywin123/search.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {boolean}
+    def search(self, nums, target):
+        if target in nums:
+            return True
+        else:
+            return False
