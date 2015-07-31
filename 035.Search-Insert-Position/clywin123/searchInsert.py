74de05d1a073f081a5526aa7d2d02857497282b3
clywin123 <clywin123@gmail.com>
2015-07-31 16:32:37 +0800

    Create searchInsert.py

---
+++ 035.Search-Insert-Position/clywin123/searchInsert.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer}
+    def searchInsert(self, nums, target):
+        for tmp in nums:
+            if(tmp>=target):
+                return nums.index(tmp)
+        return len(nums)
+        
